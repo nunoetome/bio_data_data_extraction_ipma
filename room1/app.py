f5122aa66443239bc891b3b4cec5956e2d0c83ab
Nuno Tomé <nunotomeit@gmail.com>
2024-10-02 07:12:44 +0100

    logging system finished for now TODO: transfer all log configurations to file

--- room1/app.py
+++ room1/app.py
@@ -10,11 +10,23 @@ import configparser
 #
 # Config here the logging level
 # Just uncomment the desired level and comment the others
-log_level = logging.DEBUG
-#log_level = logging.INFO
-#log_level = logging.WARNING
-#log_level = logging.ERROR
-#log_level = logging.CRITICAL
+LOG_LEVEL_GLOBAL = logging.DEBUG
+#LOG_LEVEL_GLOBAL = logging.INFO
+#LOG_LEVEL_GLOBAL = logging.WARNING
+#LOG_LEVEL_GLOBAL = logging.ERROR
+#LOG_LEVEL_GLOBAL = logging.CRITICAL
+
+#LOG_LEVEL_FILE = logging.DEBUG
+LOG_LEVEL_FILE = logging.INFO
+#LOG_LEVEL_FILE = logging.WARNING
+#LOG_LEVEL_FILE = logging.ERROR
+#LOG_LEVEL_FILE = logging.CRITICAL
+
+#LOG_LEVEL_CONSOLE = logging.DEBUG
+#LOG_LEVEL_CONSOLE = logging.INFO
+#LOG_LEVEL_CONSOLE = logging.WARNING
+LOG_LEVEL_CONSOLE = logging.ERROR
+#LOG_LEVEL_CONSOLE = logging.CRITICAL
 
 LOG_OUTPUT_FILE = 'app.log'
 
@@ -26,10 +38,28 @@ SETTINGS_FILE_PATH = 'settings.json'
 LOGGER = logging.getLogger(__name__)
 
 def ini_logging():
-    logging.basicConfig(level=log_level)
-   # logging.basicConfig(level=log_level)
-   # LOGGER = logging.getLogger(__name__)
-   # LOGGER.info('Logging configured')
+    
+    # GLOBAL LOGGING CONFIGURATION
+    LOGGER.setLevel(LOG_LEVEL_GLOBAL) 
+
+    LOGGER.debug('Application loading: log started')
+    
+    # FILE HANDLER LOGGING CONFIGURATION
+    file_handler = logging.FileHandler(LOG_OUTPUT_FILE)
+    file_handler.setLevel(LOG_LEVEL_FILE)
+    file_handler_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')        
+    file_handler.setFormatter(file_handler_formatter)
+    LOGGER.addHandler(file_handler)
+    LOGGER.debug('Application loading: log started to file')
+    
+    # CONSOLE HANDLER LOGGING CONFIGURATION
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.WARNING)
+    #console_handler_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')        
+    #console_handler.setFormatter(console_handler_formatter)
+    LOGGER.addHandler(console_handler)
+    LOGGER.debug('Application loading: log started to console')
+    
     return LOGGER
 
 
@@ -43,11 +73,11 @@ def load_settings():
 def main():
     settings = load_settings()
     ini_logging()
+    LOGGER.critical("------------------")
     LOGGER.info('Application is starting...')
     LOGGER.debug(f'Settings loaded: {settings}')     
 
-  
-    
+
     
     LOGGER.info('Finished')
     
