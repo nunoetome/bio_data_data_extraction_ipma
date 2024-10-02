aa1dfd0b38d38aadbda1900fc71935040b872fa1
Nuno Tomé <nunotomeit@gmail.com>
2024-10-02 05:07:55 +0100

    settings file implemented in json

--- room1/app.py
+++ room1/app.py
@@ -1,18 +1,47 @@
 
 # app.py
 import logging
-import mylib
+#import mylib
+import json
+import configparser
 
 logger = logging.getLogger(__name__)
 
+
+# In-code configuration
+#
+# Config here the logging level
+log_level = logging.INFO
+# log_level = logging.DEBUG
+# log_level = logging.WARNING
+# log_level = logging.ERROR
+# log_level = logging.CRITICAL
+
+SETTINGS_FILE_PATH = 'settings.json'
+
+
+
+
+def load_settings():
+    with open(SETTINGS_FILE_PATH, 'r') as file:
+        settings = json.load(file)
+    return settings
+
+
 def main():
+
     logger.info('Started')
-    mylib.do_something()
-    logger.info('Finished')
     
+    logger.info('Finished')
+
+    load_settings()
+
+    settings = load_settings()
+    logger.info(f'Settings loaded: {settings}')
     
     
     
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=log_level)
+    logger.info('Application is starting...')
     main()
